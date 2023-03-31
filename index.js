087e21ea2990b6723bb914a94cedc789daedcf78
kibiwotkosgei <collinsboen@gmail.com>
2023-03-31 16:11:05 +0300

    funtionality

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const bars = document.querySelector(".graph");
+const days = ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat'];
+const today = new Date().getDay();
+const dayName = days[today];
+const barsDiv = document.querySelector(".bar");
 
-const day = new Date().getDay();
-
-console.log(day);
+console.log(dayName);
 
 fetch("./data.json").then(res => res.json()).then(data => {
  
  bars.innerHTML +=   data.map(bar => {
- return (
-    `<div class="bar" style="height:${bar.amount*3}px"> </div>
-    <div class="day"><p> ${bar.day} </p> </div>
-    `
- )
+    
+    const kibi = bar.day;
+    
+    
+    return (
+       `<div class="bar" style="height:${bar.amount*3}px"> </div>
+       <div class="day"><p> ${bar.day} </p> </div>
+       `
+       )
 
-})
+function weekdayFuntion(){
+
+   if (dayName === kibi){
+      
+   barsDiv.classList.add("activeday")
+   
+   } 
+}
+
+
+       
+      })
 console.log(data);
 });

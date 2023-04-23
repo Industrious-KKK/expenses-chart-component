4075f4593708f66c732b4c650292698fc4a9aa15
Lesley <leskimuti@gmail.com>
2023-04-23 11:51:44 +0300

    Added the hover states on the bars

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const bars = document.querySelector(".graph");
-const days = ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat'];
+const days = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"];
 const today = new Date().getDay();
 const dayName = days[today];
-const barsDiv = document.querySelector(".bar");
 
-console.log(dayName);
-
-fetch("./data.json").then(res => res.json()).then(data => {
- 
- bars.innerHTML +=   data.map(bar => {
-    
-
-    
-    
-    return (
-       `<div class="bar_items">
-       <div class=${bar.day === dayName ? "activeday" : "bar"} style="height:${bar.amount*3}px"> </div>
-       <p class="day"> ${bar.day} </p>
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    bars.innerHTML += data
+      .map((bar) => {
+        return `<div class="bar_items">
+        <div class=${
+          bar.day === dayName ? "activeday" : "bar"
+        } style="height:${bar.amount * 3}px"> </div>
+        <p class="amount">$${bar.amount}</p>
+        <p class="day"> ${bar.day} </p>
        </div>
-       `
-       )
-
-      }).join("")
-      console.log(data);
-   });
-
-   
-console.log(today);
+       `;
+      })
+      .join("");
+  });
